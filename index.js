a04879409ac8c855a9d051ed425b45b7bada8077
kash-jay <kp2681@nyu.edu>
2023-11-28 22:16:18 +0400

    Added modals for character videos

--- index.js
+++ index.js
@@ -18,4 +18,24 @@ window.addEventListener("scroll", scrollAnim);
 
 window.addEventListener('DOMContentLoaded', () => {
     palmville.classList.add('loaded');
-})
+});
+
+var modal = document.getElementById("myModal");
+var btns = document.querySelectorAll(".res-btn");
+var span = document.getElementsByClassName("close")[0];
+btns.forEach(function(btn) {
+  btn.onclick = function() {
+    console.log(btn.id);
+    modal.style.display = "flex";
+  }
+});
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
